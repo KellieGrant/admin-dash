6a550c1db361f758ec4ba89955930ceec46c27e6
KeltaDawn <kellieleagrant@gmail.com>
2025-01-18 10:58:15 -0500

    Post page created, Edit post also created.

---
+++ app/(main)/posts/edit/[id]/page.tsx
@@ -0,0 +1,160 @@
+"use client";
+
+import BackButton from "@/components/ui/BackButton";
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { Button } from "@/components/ui/button";
+import posts from "@/data/posts";
+import { use, useState, useEffect } from "react";
+
+const formSchema = z.object({
+  title: z.string().min(1, {
+    message: "Title is required",
+  }),
+  body: z.string().min(1, {
+    message: "Body is required",
+  }),
+  Author: z.string().min(1, {
+    message: "Author is required",
+  }),
+  Date: z.string().min(1, {
+    message: "Date is required",
+  }),
+});
+
+interface PostEditPageProps {
+  params: Promise<{ id: string }>;
+}
+
+const PostEditPage = ({ params }: PostEditPageProps) => {
+  const [post, setPost] = useState<any | null>(null);
+  const { id } = use(params);
+
+  useEffect(() => {
+    const foundPost = posts.find((p) => p.id === id);
+    setPost(foundPost);
+  }, [id]);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: posts
+      ? {
+          title: post?.title || "",
+          body: post?.body || "",
+          Author: post?.author || "",
+          Date: post?.date || "",
+        }
+      : { title: "", body: "", Author: "", Date: "" },
+  });
+
+  const handleSubmit = (data: z.infer<typeof formSchema>) => {
+    console.log(data);
+  };
+
+  return (
+    <>
+      <BackButton text='Back to Post' link='/posts' />
+      <h3 className='text-2xl mb-4'>Edit Post</h3>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(handleSubmit)} className='space-y-8'>
+          <FormField
+            control={form.control}
+            name='title'
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className='uppercase text-xs font-bold text-zinc-500 dark:text-white'>
+                  Title
+                </FormLabel>
+                <FormControl>
+                  <Input
+                    className='bg-slate-100 dark:bg-slate-500 border-0 focus-visible:ring-0 text-black drak:text-white focus-visible:ring-offset-0'
+                    placeholder='Enter Title'
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name='body'
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className='uppercase text-xs font-bold text-zinc-500 dark:text-white'>
+                  Body
+                </FormLabel>
+                <FormControl>
+                  <Textarea
+                    className='bg-slate-100 dark:bg-slate-500 border-0 focus-visible:ring-0 text-black drak:text-white focus-visible:ring-offset-0'
+                    placeholder='Body'
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name='Author'
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className='uppercase text-xs font-bold text-zinc-500 dark:text-white'>
+                  Author
+                </FormLabel>
+                <FormControl>
+                  <Input
+                    className='bg-slate-100 dark:bg-slate-500 border-0 focus-visible:ring-0 text-black drak:text-white focus-visible:ring-offset-0'
+                    placeholder='Author'
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name='Date'
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className='uppercase text-xs font-bold text-zinc-500 dark:text-white'>
+                  Date
+                </FormLabel>
+                <FormControl>
+                  <Input
+                    className='bg-slate-100 dark:bg-slate-500 border-0 focus-visible:ring-0 text-black drak:text-white focus-visible:ring-offset-0'
+                    placeholder='Date'
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <Button className='w-full dark:slate-800 dark:text-white'>
+            Update Post
+          </Button>
+        </form>
+      </Form>
+    </>
+  );
+};
+
+export default PostEditPage;
